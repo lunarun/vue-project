5c6bdb72e12685d2f97688841ead0d32b670fb96
lunacat <706827455@qq.com>
2017-03-02 18:05:25 +0800

    layout vue temp

--- build/dev-server.js
+++ build/dev-server.js
@@ -29,6 +29,28 @@ var goods = appData.goods
 var ratings = appData.ratings
 
 var apiRoutes = express.Router()
+apiRoutes.get('/seller', function(req, res){
+  res.json({
+    errno: 0,
+    data: seller
+  })
+})
+
+apiRoutes.get('/goods', function(req, res){
+  res.json({
+    errno: 0,
+    data: goods
+  })
+})
+
+apiRoutes.get('/ratings', function(req, res){
+  res.json({
+    errno: 0,
+    data: ratings
+  })
+})
+
+app.use('/api',apiRoutes);
 
 var compiler = webpack(webpackConfig)
 
---
+++ src/components/Home/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="home">
+    <home-header></home-header>
+    <home-content></home-content>
+  </div>
+</template>
+
+<script>
+  import HomeHeader from './HomeHeader'
+  import HomeContent from './HomeContent'
+
+  export default {
+    name: 'home',
+    data () {
+      return {
+      }
+    },
+    components: {
+      HomeHeader,
+      HomeContent
+    }
+  }
+</script>
+
+<style>
+
+</style>
